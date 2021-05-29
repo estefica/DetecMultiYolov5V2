d0f36098ce16430a1b4330025d96ba7b95f44f88
estefica <80427609+estefica@users.noreply.github.com>
2021-05-28 21:56:28 -0500

    Update general.py

--- utils/general.py
+++ utils/general.py
@@ -124,7 +124,8 @@ def check_img_size(img_size, s=32):
     # Verify img_size is a multiple of stride s
     new_size = make_divisible(img_size, int(s))  # ceil gs-multiple
     if new_size != img_size:
-        print('WARNING: --img-size %g must be multiple of max stride %g, updating to %g' % (img_size, s, new_size))
+        pass
+        #print('WARNING: --img-size %g must be multiple of max stride %g, updating to %g' % (img_size, s, new_size))
     return new_size
 
 
