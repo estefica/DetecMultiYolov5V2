23c88a7563f9672ee09af1bc2ba3532955371b6d
estefica <80427609+estefica@users.noreply.github.com>
2021-06-01 12:32:47 -0500

    Update data_img_v1.py

--- data_img_v1.py
+++ data_img_v1.py
@@ -1,6 +1,9 @@
 import os, shutil,cv2
 import numpy as np
-path_images =  'C:/Users/carit/PycharmProjects/tutorial/data_2/'
+
+path_save_images = '/content/random/images/'#'C:/Users/carit/PycharmProjects/tutorial/carpetadata/'#
+path_save_labels = '/content/random/labels/'
+path_images =  '/content/prueba_data/'
 lista_read = os.listdir(path_images)
 
 def center_dimensions(labels,ancho,alto):
@@ -48,64 +51,58 @@ def crop_limits(p1x_l, p1y_l, p2x_l, p2y_l,x_aumt,y_aumt):
     if limy2 > alto:
         limy2 = alto
     return limx1,limx2,limy1,limy2,x_ch,y_ch
-for f in os.listdir(path_images):
-    if f.endswith('.jpg'):
-        ptos1x = []
-        ptos1y = []
-        ptos2x = []
-        ptos2y = []
 
-        border_all = {}
 
-        fn, ftext = os.path.splitext(f)
-        if os.path.exists(path_images + fn + '.txt'):
-            img = cv2.imread(path_images + f'{fn}.jpg')
-            f = open(path_images + fn + '.txt')
-            labels = np.array([x.split() for x in f.read().strip().splitlines()],
-                              dtype=np.float32)  # label
-            if labels.shape[0] >= 2:
-                final =np.zeros((labels.shape[0],5))
-                for boxes in range(labels.shape[0]):
-                    ancho = img.shape[1]
-                    alto = img.shape[0]
-                    cx,cy,w,h,objeto =center_dimensions(labels[boxes],ancho,alto)
-                    p1x,p1y,p2x,p2y = borders(cx,cy,w,h)
-                    ptos1x.append(p1x)
-                    ptos1y.append(p1y)
-                    ptos2x.append(p2x)
-                    ptos2y.append(p2y)
-                    border_all[boxes] = [cx,cy,w,h,objeto]
-                p1x_l, p1y_l, p2x_l, p2y_l = points_borders(ptos1x,ptos1y,ptos2x,ptos2y)
-                #definicion de limites recorte
-                x_aumt = int(0.02*ancho)
-                y_aumt = int(0.02*alto)
-                limx1,limx2,limy1,limy2,x_ch,y_ch = crop_limits(p1x_l, p1y_l, p2x_l, p2y_l,x_aumt,y_aumt)
-                ancho_new = limx2 - limx1
-                alto_new = limy2 - limy1
-                img_resize = img[limy1:limy2,limx1:limx2]
-                cv2.imwrite('d2/MIRA.jpg', img_resize)
-                print(border_all)
-                for key in border_all.keys():
-                    cx = border_all[key][0]
-                    cy = border_all[key][1]
-                    w_o = border_all[key][2]
-                    h_o = border_all[key][3]
-                    if x_ch ==0:
-                        cx = cx - limx1
-                    if y_ch ==0:
-                        cy = cy - limy1
-                    #cambio formato
-                    cx = cx/ancho_new
-                    cy = cy/alto_new
-                    w = w_o/ancho_new
-                    h = h_o/alto_new
-                    final[key] = [objeto,cx,cy,w,h]
-                doc = open('d2/MIRA.txt', 'w')
-                for n in range(final.shape[0]):
-                    objetost=int(final[n][0])
-                    cx =final[n][1]
-                    cy =final[n][2]
-                    w =final[n][3]
-                    h =final[n][4]
-                    final_alt = '{} {} {} {} {}\n'.format(objetost, cx,cy,w,h)
-                    doc.write(final_alt)
+def all_objects(img_d,labels,name):
+    ptos1x = []
+    ptos1y = []
+    ptos2x = []
+    ptos2y = []
+    border_all = {}
+    img = img_d
+    if labels.shape[0] >= 2:
+        final =np.zeros((labels.shape[0],5))
+        for boxes in range(labels.shape[0]):
+            ancho = img.shape[1]
+            alto = img.shape[0]
+            cx,cy,w,h,objeto =center_dimensions(labels[boxes],ancho,alto)
+            p1x,p1y,p2x,p2y = borders(cx,cy,w,h)
+            ptos1x.append(p1x)
+            ptos1y.append(p1y)
+            ptos2x.append(p2x)
+            ptos2y.append(p2y)
+            border_all[boxes] = [cx,cy,w,h,objeto]
+        p1x_l, p1y_l, p2x_l, p2y_l = points_borders(ptos1x,ptos1y,ptos2x,ptos2y)
+        #definicion de limites recorte
+        x_aumt = int(0.02*ancho)
+        y_aumt = int(0.02*alto)
+        limx1,limx2,limy1,limy2,x_ch,y_ch = crop_limits(p1x_l, p1y_l, p2x_l, p2y_l,x_aumt,y_aumt)
+        ancho_new = limx2 - limx1
+        alto_new = limy2 - limy1
+        img_resize = img[limy1:limy2,limx1:limx2]
+        cv2.imwrite(path_save_images+name+'_all_b'+'.jpg', img_resize)
+        #print(border_all)
+        for key in border_all.keys():
+            cx = border_all[key][0]
+            cy = border_all[key][1]
+            w_o = border_all[key][2]
+            h_o = border_all[key][3]
+            if x_ch ==0:
+                cx = cx - limx1
+            if y_ch ==0:
+                cy = cy - limy1
+            #cambio formato
+            cx = cx/ancho_new
+            cy = cy/alto_new
+            w = w_o/ancho_new
+            h = h_o/alto_new
+            final[key] = [objeto,cx,cy,w,h]
+        doc = open(path_save_labels+name+'_all_b'+'.txt', 'w')
+        for n in range(final.shape[0]):
+            objetost=int(final[n][0])
+            cx =final[n][1]
+            cy =final[n][2]
+            w =final[n][3]
+            h =final[n][4]
+            final_alt = '{} {} {} {} {}\n'.format(objetost, cx,cy,w,h)
+            doc.write(final_alt)
