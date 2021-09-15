bf4ac405168e3de8b110957ba7161aa6927bd77e
estefica <80427609+estefica@users.noreply.github.com>
2021-09-15 15:12:23 -0500

    Update general.py
    Actualizacion_comentarios

--- utils/general.py
+++ utils/general.py
@@ -419,6 +419,7 @@ def wh_iou(wh1, wh2):
     return inter / (wh1.prod(2) + wh2.prod(2) - inter)  # iou = inter / (area1 + area2 - inter)
 
 
+
 def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=None, agnostic=False, multi_label=False,
                         labels=()):
     """Runs Non-Maximum Suppression (NMS) on inference results
@@ -428,6 +429,7 @@ def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=Non
     """
 
     nc = prediction.shape[2] - 5  # number of classes
+
     xc = prediction[..., 4] > conf_thres  # candidates
 
     # Settings
@@ -508,6 +510,137 @@ def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=Non
             break  # time limit exceeded
 
     return output
+
+def escalado(box,x_cor,y_cor,escalay,escalax):
+    # [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    y = box.clone() if isinstance(box, torch.Tensor) else np.copy(box)
+    #print(f'x: {x_cor},y :{y_cor}')
+    
+    y[:, 0] = box[:, 0] + x_cor  # borde superior X
+    y[:, 1] = box[:, 1] + y_cor  # borde superior y
+    y[:, 2] = box[:, 2] + x_cor  # borde inferior x
+    y[:, 3] = box[:, 3] + y_cor  # borde inferior y
+    y[:, 0] *= escalax
+    y[:, 1] *= escalay
+    y[:, 2] *= escalax
+    y[:, 3] *= escalay
+
+    return y
+
+def non_max_suppression_inf(alton,anchon,prediction,p_out,flag_im,x_cor,y_cor,escalay,escalax,split_count,conf_thres=0.25, iou_thres=0.45, classes_inf=None, agnostic_nms_inf=False, multi_label=False,
+                        labels=()):
+    """Runs Non-Maximum Suppression (NMS) on inference results
+
+    Returns:
+         list of detections, on (n,6) tensor per image [xyxy, conf, cls]
+    """
+
+    nc = prediction.shape[2] - 5  # number of classes
+    if alton<3500 and anchon<3500:
+          conf_thres = 0.9#0.85#0.8#0.93
+          if alton<2500 and anchon<2500:
+            conf_thres = 0.9#0.8#0.8#0.93
+            if alton<1600 and anchon<1600:
+              #print('\n\n ENTRE AMIWIIIIIII\n')
+              conf_thres = 0.7#0.85#0.7#0.78
+              if alton<1200 and anchon<1200:
+                #print('\n\n ENTRE AMIWIIIIIII\n')
+                conf_thres = 0.7#0.7#0.9#0.60
+    
+        
+    xc = prediction[..., 4] > conf_thres  # candidates
+
+    # Settings
+    min_wh, max_wh = 2, 4096  # (pixels) minimum and maximum box width and height
+    max_det = 300  # maximum number of detections per image
+    max_nms = 30000  # maximum number of boxes into torchvision.ops.nms()
+    time_limit = 10.0  # seconds to quit after
+    redundant = True  # require redundant detections
+    multi_label &= nc > 1  # multiple labels per box (adds 0.5ms/img)
+    merge = False  # use merge-NMS
+    t = time.time()
+    output = [torch.zeros((0, 6), device=prediction.device)] * prediction.shape[0]
+    for xi, x in enumerate(prediction):  # image index, image inference
+        # Apply constraints
+        # x[((x[..., 2:4] < min_wh) | (x[..., 2:4] > max_wh)).any(1), 4] = 0  # width-height
+        x = x[xc[xi]]  # confidence
+
+        # Cat apriori labels if autolabelling
+        if labels and len(labels[xi]):
+            l = labels[xi]
+            v = torch.zeros((len(l), nc + 5), device=x.device)
+            v[:, :4] = l[:, 1:5]  # box
+            v[:, 4] = 1.0  # conf
+            v[range(len(l)), l[:, 0].long() + 5] = 1.0  # cls
+            x = torch.cat((x, v), 0)
+
+        # If none remain process next image
+        if not x.shape[0] and flag_im==0:
+            continue
+
+        # Compute conf
+        x[:, 5:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
+
+        # Box (center x, center y, width, height) to (x1, y1, x2, y2)
+        box = xywh2xyxy(x[:, :4])#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<--------------PRIMERA DETECCION X------------------
+        box = escalado(box,x_cor,y_cor,escalay,escalax)
+        # Detections matrix nx6 (xyxy, conf, cls)
+        if multi_label:
+            #print('\n\n entre multi label\n')
+            i, j = (x[:, 5:] > conf_thres).nonzero(as_tuple=False).T
+            x = torch.cat((box[i], x[i, j + 5, None], j[:, None].float()), 1)
+        else:  # best class only
+            conf, j = x[:, 5:].max(1, keepdim=True)
+            x = torch.cat((box, conf, j.float()), 1)[conf.view(-1) > conf_thres]
+        if flag_im ==0 and split_count==1: #<<<<<<<<<<<<<<<<<<<<<<<<----------------------ALMACENO EN POUT LAS DETECCIONES --1RA VEZ--
+            p_out = x
+
+        if split_count !=1:
+            p_out = torch.vstack((p_out,x)) # #<<<<<<<<<<<<<<<<<<<<<<<<----------------------ALMACENO EN POUT LAS DETECCIONES --2DA VEZ--
+            
+        if flag_im !=1:
+            continue
+        if flag_im ==1:
+            
+            x =  p_out#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<---------------------ALMACENO EN POUT LAS DETECCIONES--ULTIMA VEZ--
+        # Filter by class
+        if classes_inf is not None:
+            #print('\n\n filter clases\n')
+            x = x[(x[:, 5:6] == torch.tensor(classes_inf, device=x.device)).any(1)]
+
+        # Apply finite constraint
+        # if not torch.isfinite(x).all():
+        #     x = x[torch.isfinite(x).all(1)]
+
+        # Check shape
+        n = x.shape[0]  # number of boxes
+        if not n:  # no boxes
+            continue
+        elif n > max_nms:  # excess boxes
+            x = x[x[:, 4].argsort(descending=True)[:max_nms]]  # sort by confidence
+
+        # Batched NMS
+        c = x[:, 5:6] * (0 if agnostic_nms_inf else max_wh)  # classes
+        boxes, scores = x[:, :4] + c, x[:, 4]  # boxes (offset by class), scores
+        i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<--------------- FINAL DE LAS DETECCIONES
+        
+        if i.shape[0] > max_det:  # limit detections
+            i = i[:max_det]
+        if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
+            # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
+            
+            iou = box_iou(boxes[i], boxes) > iou_thres  # iou matrix
+            weights = iou * scores[None]  # box weights
+            x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(1, keepdim=True)  # merged boxes
+            if redundant:
+                i = i[iou.sum(1) > 1]  # require redundancy
+
+        output[xi] = x[i]
+        if (time.time() - t) > time_limit:
+            print(f'WARNING: NMS time limit {time_limit}s exceeded')
+            break  # time limit exceeded
+    return output,p_out
+
 
 
 def strip_optimizer(f='best.pt', s=''):  # from utils.general import *; strip_optimizer()
